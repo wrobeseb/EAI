34e4b689304f59c86b361f72a0cd6762c119bdfa
sao <sao@sao-linuks.(none)>
2011-05-02 23:18:17 +0200

    Poprawki pozadkowe...

--- servicemix/EAI-CAMEL-ROUTER/EAI-CAMEL-ROUTER-SU/src/main/java/pwr/tin/tip/sw/pd/eai/Router.java
+++ servicemix/EAI-CAMEL-ROUTER/EAI-CAMEL-ROUTER-SU/src/main/java/pwr/tin/tip/sw/pd/eai/Router.java
@@ -3,6 +3,7 @@ package pwr.tin.tip.sw.pd.eai;
 import org.apache.camel.Body;
 import org.apache.camel.Consume;
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.RecipientList;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
@@ -12,20 +13,21 @@ public class Router {
 
 	public final static String JBI_PREFIX = "jbi:endpoint:http://pwr.tin.tip.sw.pd.eai";
 	
-	@Consume(uri=JBI_PREFIX + "/kernelDSLRouter/EAI")
+	@Consume(uri=JBI_PREFIX + "/cuDSLRouter/EAI")
 	@RecipientList
-	public String route(@Body String body, Exchange exchange){
+	public String route(Exchange exchange){
+		/*String body = (String) exchange.getIn().getBody();
 		String algorithmName = getString(body, "/algorithm/algorithmName");
 		String algorithmEndpoint = getString(body, "/algorithm/algorithmEndpoint");
-		if (algorithmEndpoint != null) {
-			return JBI_PREFIX + "/algorithmsInService/EAI";
-		}
+		if (algorithmEndpoint != null) {*/
+			return JBI_PREFIX + "/euInService/EAI";
+		/*}
 		else {
 			if (algorithmName != null) {
-				return JBI_PREFIX + "/algorithmsInService/EAI";
+				return JBI_PREFIX + "/euInService/EAI";
 			}
 		}
-		return null;
+		return null;*/
 	}
 	
 	private String getString(String messageBody, String xpathExpression) {
