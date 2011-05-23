0b0479ac063a3fa83a1dab96338513fd82bb4203
Sairo <wrobel.sebastian@gmail.com>
2011-05-24 00:46:51 +0200

    Konfiguracja routera DSL pod kontem dostepu do bazy danych

--- servicemix/EAI-CAMEL-ROUTER/EAI-CAMEL-ROUTER-SU/src/main/java/pwr/tin/tip/sw/pd/eai/Router.java
+++ servicemix/EAI-CAMEL-ROUTER/EAI-CAMEL-ROUTER-SU/src/main/java/pwr/tin/tip/sw/pd/eai/Router.java
@@ -1,17 +1,23 @@
 package pwr.tin.tip.sw.pd.eai;
 
-import org.apache.camel.Body;
 import org.apache.camel.Consume;
 import org.apache.camel.Exchange;
-import org.apache.camel.Message;
 import org.apache.camel.RecipientList;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Node;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import pwr.tin.tip.sw.pd.eai.service.IRouterService;
+
+@Component(value="router")
 public class Router {
 
 	public final static String JBI_PREFIX = "jbi:endpoint:http://pwr.tin.tip.sw.pd.eai";
+	
+	@Autowired(required=true)
+	private IRouterService routerService;
 	
 	@Consume(uri=JBI_PREFIX + "/cuDSLRouter/EAI")
 	@RecipientList
@@ -20,6 +26,7 @@ public class Router {
 		String algorithmName = getString(body, "/algorithm/algorithmName");
 		String algorithmEndpoint = getString(body, "/algorithm/algorithmEndpoint");
 		if (algorithmEndpoint != null) {*/
+			routerService.getLoadInfo();
 			return JBI_PREFIX + "/euInService/EAI";
 		/*}
 		else {
